6f66fa1d5d3daaea2319e047f61eb1a869ab7c3e
oscar08850 <oscar08850@gmail.com>
2021-12-16 23:52:29 +0100

    Bares de prueba agregados al mapa

--- lib/Screens/Map/ui/pages/home/home_controller.dart
+++ lib/Screens/Map/ui/pages/home/home_controller.dart
@@ -6,7 +6,11 @@ import 'package:flutter/cupertino.dart' show ChangeNotifier;
 import 'package:flutter_auth/Screens/Map/ui/utils/map_style.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class HomeController extends ChangeNotifier{
+class Vectores {
+
+}
+
+class HomeController extends ChangeNotifier {
   final Map<MarkerId, Marker> _markers = {};
 
   Set<Marker> get markers => _markers.values.toSet();
@@ -19,32 +23,46 @@ class HomeController extends ChangeNotifier{
     target: LatLng(41.2756656, 1.9859211), //EETAC: 41.2756656,1.9859211
     zoom: 15,
   );
- 
+
   void onMapCreatedyeah(GoogleMapController controller) {
     controller.setMapStyle(mapStyle);
   }
 
 
 
-  void onTap(LatLng position) {
+
+  void createMarkers(int num) {
+
     final id = _markers.length.toString();
-    final markerId = MarkerId(id);
-    LatLng pepe = new LatLng(41.2745368,1.9834524); //Residencia universitaria EETAC
-    final marker = Marker(
-      markerId: markerId,
-      position: (pepe), //(position)
-      onTap: (){
-        _markersController.sink.add(id);
-      }
-    );
-    _markers[markerId] = marker;
-    notifyListeners();
+    //final markerId = MarkerId(id);
+    LatLng pepe = new LatLng(41.2745368, 1.9834524); //Residencia universitaria EETAC
+    LatLng pepe2 = new LatLng(41.279730, 1.983748); //Residencia universitaria EETAC
+    LatLng pepe3 = new LatLng(41.2735649, 1.9799459); //Residencia universitaria EETAC
+    LatLng pepe4 = new LatLng(41.2724051, 1.9721655); //Residencia universitaria EETAC
+    List marcadores = [pepe,pepe2,pepe3,pepe4];
+
+
+    for (int i = 0; i < num; i++) {
+      final markerId = MarkerId(i.toString());
+      final marker = Marker(
+          markerId: markerId,
+          position: (marcadores[i]), //(position)
+          onTap: () {
+            _markersController.sink.add(i.toString());
+            //Console.log(i);
+          });
+      _markers[markerId] = marker;
+      notifyListeners();
+    }
   }
 
+  void onTap(LatLng position) {
+    createMarkers(4);
+  }
 
   @override
   void dispose() {
     _markersController.close();
     super.dispose();
   }
-} 
+}
