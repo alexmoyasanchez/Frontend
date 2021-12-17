185f75340873842c56bbc5e8c8de45af25fcdf3e
unknown <jramirezsamc@gmail.com>
2021-12-17 03:50:41 +0100

    stable version WIP

--- lib/Screens/Chat/components/body.dart
+++ lib/Screens/Chat/components/body.dart
@@ -5,6 +5,7 @@ import 'package:flutter_auth/data/data.dart';
 import 'package:flutter_auth/models/bar_model.dart';
 import 'package:flutter_auth/constants.dart';
 import 'dart:async';
+import 'package:intl/intl.dart';
 import 'package:flutter_auth/Screens/ChatList/chatList_screen.dart';
 import 'package:flutter_auth/Screens/BarList/BarList_screen.dart';
 import 'package:flutter_auth/Screens/UserList/components/background.dart';
@@ -19,87 +20,52 @@ class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     String text = "";
-    return Container(
-      width: double.infinity,
-      decoration: BoxDecoration(
-          gradient: LinearGradient(
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-              colors: [
-            Color(0x66ff56d4),
-            Color(0x99ff56d4),
-            Color(0xccff56d4),
-            Color(0xffff56d4),
-          ])),
-      child: Stack(
-        alignment: Alignment.center,
-        children: <Widget>[
-          Align(
-            alignment: Alignment.bottomLeft,
-            child: Container(
-              padding: EdgeInsets.only(left: 10, bottom: 10, top: 10),
-              height: 60,
-              width: double.infinity,
-              color: Colors.white,
-              child: Row(
-                children: <Widget>[
-                  GestureDetector(
-                    onTap: () {},
-                    child: Container(
-                      height: 30,
-                      width: 30,
-                      decoration: BoxDecoration(
-                        color: Colors.lightBlue,
-                        borderRadius: BorderRadius.circular(30),
-                      ),
-                      child: Icon(
-                        Icons.add,
-                        color: Colors.white,
-                        size: 20,
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    width: 15,
-                  ),
-                  Expanded(
-                    child: TextField(
-                      onChanged: (value) {
-                        text = value;
+    DateTime now = DateTime.now();
+    DateFormat formatter = DateFormat('Hm');
+    String formatted = formatter.format(now);
+    String name = currentUser.username;
+    return Background(
+        child: Center(
+            child: FutureBuilder(
+                future: getMessages(),
+                builder: (BuildContext context, AsyncSnapshot snapshot) {
+                  print(snapshot.data);
+                  if (snapshot.data == null) {
+                    return Container(
+                        child: Center(child: CircularProgressIndicator()));
+                  } else {
+                    return ListView.builder(
+                      itemCount: snapshot.data.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return ListTile(
+                          title: Text(snapshot.data[index].text,
+                              style: TextStyle(
+                                  color: Colors.pink[800],
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold)),
+                          subtitle: Text(
+                              'Enviado por: ' +
+                                  snapshot.data[index].sender +
+                                  ' a las ' +
+                                  snapshot.data[index].time,
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.bold)),
+                        );
                       },
-                      decoration: InputDecoration(
-                          hintText: "Write message...",
-                          hintStyle: TextStyle(color: Colors.black54),
-                          border: InputBorder.none),
-                    ),
-                  ),
-                  SizedBox(
-                    width: 15,
-                  ),
-                  FloatingActionButton(
-                    child: Icon(
-                      Icons.send,
-                      color: Colors.white,
-                      size: 18,
-                    ),
-                    onPressed: () {
-                      SendMessage(currentUser.name, text, "ara mismo");
-                    },
-                    backgroundColor: Colors.blue,
-                    elevation: 0,
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
+                    );
+                  }
+                })));
   }
 }
 
 _buildMessageComposer() {
   String text = "";
+  DateTime now = DateTime.now();
+  DateFormat formatter = DateFormat('Hm');
+  String formatted = formatter.format(now);
+  String name = currentUser.name;
   return Container(
     height: 70.0,
     color: Colors.white,
@@ -128,10 +94,48 @@ _buildMessageComposer() {
           iconSize: 25.0,
           color: Colors.blueAccent,
           onPressed: () {
-            EnviarMensaje(currentUser.name, text, "22/2/12");
+            EnviarMensaje(name, text, formatted);
           },
         ),
       ],
     ),
   );
+}
+
+_showMessages() {
+  Widget build(BuildContext context) {
+    child:
+    Center(
+        child: FutureBuilder(
+            future: getMessages(),
+            builder: (BuildContext context, AsyncSnapshot snapshot) {
+              print(snapshot.data);
+              if (snapshot.data == null) {
+                return Container(
+                    child: Center(child: CircularProgressIndicator()));
+              } else {
+                return ListView.builder(
+                  itemCount: snapshot.data.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return ListTile(
+                      title: Text(snapshot.data[index].text,
+                          style: TextStyle(
+                              color: Colors.pink[800],
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold)),
+                      subtitle: Text(
+                          'Enviado por: ' +
+                              snapshot.data[index].sender +
+                              'A las ' +
+                              snapshot.data[index].time,
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 15,
+                              fontWeight: FontWeight.bold)),
+                    );
+                  },
+                );
+              }
+            }));
+  }
 }
