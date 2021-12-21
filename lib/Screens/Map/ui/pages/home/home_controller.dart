657b09b2390bb4cd1669ad9f790907745bb10a96
oscar08850 <oscar08850@gmail.com>
2021-12-21 15:02:52 +0100

    first commit

--- lib/Screens/Map/ui/pages/home/home_controller.dart
+++ lib/Screens/Map/ui/pages/home/home_controller.dart
@@ -3,12 +3,13 @@
 import 'dart:async';
 
 import 'package:flutter/cupertino.dart' show ChangeNotifier;
+import 'package:flutter_auth/Screens/BarList/BarList_screen.dart';
 import 'package:flutter_auth/Screens/Map/ui/utils/map_style.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_auth/Screens/BarList/components/body.dart';
 
-class Vectores {
-
-}
+class Vectores {}
 
 class HomeController extends ChangeNotifier {
   final Map<MarkerId, Marker> _markers = {};
@@ -28,25 +29,37 @@ class HomeController extends ChangeNotifier {
     controller.setMapStyle(mapStyle);
   }
 
-
-
-
   void createMarkers(int num) {
-
     final id = _markers.length.toString();
     //final markerId = MarkerId(id);
-    LatLng pepe = new LatLng(41.2745368, 1.9834524); //Residencia universitaria EETAC
-    LatLng pepe2 = new LatLng(41.279730, 1.983748); //Residencia universitaria EETAC
-    LatLng pepe3 = new LatLng(41.2735649, 1.9799459); //Residencia universitaria EETAC
-    LatLng pepe4 = new LatLng(41.2724051, 1.9721655); //Residencia universitaria EETAC
-    List marcadores = [pepe,pepe2,pepe3,pepe4];
-
+    LatLng pepe =
+        new LatLng(41.2745368, 1.9834524); //Residencia universitaria EETAC
+    LatLng pepe2 =
+        new LatLng(41.279730, 1.983748); //Residencia universitaria EETAC
+    LatLng pepe3 =
+        new LatLng(41.2735649, 1.9799459); //Residencia universitaria EETAC
+    LatLng pepe4 =
+        new LatLng(41.2724051, 1.9721655); //Residencia universitaria EETAC
+    List marcadores = [pepe, pepe2, pepe3, pepe4];
 
     for (int i = 0; i < num; i++) {
       final markerId = MarkerId(i.toString());
+      BuildContext context;
+      AsyncSnapshot snapshot;
       final marker = Marker(
           markerId: markerId,
           position: (marcadores[i]), //(position)
+          infoWindow: InfoWindow(
+            title: ("¿¿¿ Quieres algo más Angel ???"),
+            snippet: ("Aqui lo del cuerpo"),
+            onTap: () {
+              getBares();
+              Navigator.push(
+                  context,
+                  new MaterialPageRoute(
+                      builder: (context) => DetailPage(snapshot.data[1])));
+            },
+          ),
           onTap: () {
             _markersController.sink.add(i.toString());
             //Console.log(i);
