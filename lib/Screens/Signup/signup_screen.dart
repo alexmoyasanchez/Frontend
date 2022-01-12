51d8c126ffb877ac2f833c0208b7b99a032ab984
Angelfm98 <angel.franco@estudiantat.upc.edu>
2022-01-12 19:01:52 +0100

    update 3.0

--- lib/Screens/Signup/signup_screen.dart
+++ lib/Screens/Signup/signup_screen.dart
@@ -36,6 +36,35 @@ Future<User> createUser(String username, String password, String email) async {
   }
 }
 
+Future<User> UserAuthbyGoogle(String email, String id) async {
+  final response = await http.post(
+    Uri.parse('http://147.83.7.157:3000/usuarios/new'),
+    headers: <String, String>{
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+    body: jsonEncode(<String, String>{
+      
+      'email': email,
+      'uid': id,
+      'nombre': "",
+      'edad': "",
+      'descripcion': "",
+      'imageUrl': "",
+      'puntuacion': '0',
+    }),
+  );
+
+  if (response.statusCode == 201) {
+    // If the server did return a 201 CREATED response,
+    // then parse the JSON.
+    return User.fromJson(jsonDecode(response.body));
+  } else {
+    // If the server did not return a 201 CREATED response,
+    // then throw an exception.
+    throw Exception(S.current.wrongu);
+  }
+}
+
 class SignUpScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
--- lib/auth.dart
+++ lib/auth.dart
@@ -4,159 +4,10 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-
+import 'Screens/Signup/signup_screen.dart';
 import 'package:flutter/foundation.dart' show kIsWeb;
 
-// class AuthService {
-//   final FirebaseAuth auth = FirebaseAuth.instance;
-//   final GoogleSignIn googleSignIn = GoogleSignIn();
 
-//   String name;
-//   String imageUrl;
-
-//   String uid;
-//   String userEmail;
-
-//   Future<User> registerWithEmailPassword(String email, String password) async {
-//     // Initialize Firebase
-//     await Firebase.initializeApp();
-//     User user;
-
-//     try {
-//       UserCredential userCredential = await auth.createUserWithEmailAndPassword(
-//         email: email,
-//         password: password,
-//       );
-
-//       user = userCredential.user;
-
-//       if (user != null) {
-//         uid = user.uid;
-//         userEmail = user.email;
-//       }
-//     } on FirebaseAuthException catch (e) {
-//       if (e.code == 'weak-password') {
-//         print('The password provided is too weak.');
-//       } else if (e.code == 'email-already-in-use') {
-//         print('An account already exists for that email.');
-//       }
-//     } catch (e) {
-//       print(e);
-//     }
-
-//     return user;
-//   }
-
-//   Future<User> signInWithEmailPassword(String email, String password) async {
-//     await Firebase.initializeApp();
-//     User user;
-
-//     try {
-//       UserCredential userCredential = await auth.signInWithEmailAndPassword(
-//         email: email,
-//         password: password,
-//       );
-//       user = userCredential.user;
-
-//       if (user != null) {
-//         uid = user.uid;
-//         userEmail = user.email;
-
-//         SharedPreferences prefs = await SharedPreferences.getInstance();
-//         await prefs.setBool('auth', true);
-//       }
-//     } on FirebaseAuthException catch (e) {
-//       if (e.code == 'user-not-found') {
-//         print('No user found for that email.');
-//       } else if (e.code == 'wrong-password') {
-//         print('Wrong password provided.');
-//       }
-//     }
-
-//     return user;
-//   }
-
-//   // Future<User> signInWithGoogle() async {
-//   //   // Initialize Firebase
-//   //   await Firebase.initializeApp();
-//   //   User user;
-
-//   //   // The `GoogleAuthProvider` can only be used while running on the web
-//   //   GoogleAuthProvider authProvider = GoogleAuthProvider();
-
-//   //   try {
-//   //     final UserCredential userCredential =
-//   //         await auth.signInWithPopup(authProvider);
-
-//   //     user = userCredential.user;
-//   //   } catch (e) {
-//   //     print(e);
-//   //   }
-
-//   //   if (user != null) {
-//   //     uid = user.uid;
-//   //     name = user.displayName;
-//   //     userEmail = user.email;
-//   //     imageUrl = user.photoURL;
-
-//   //     SharedPreferences prefs = await SharedPreferences.getInstance();
-//   //     prefs.setBool('auth', true);
-//   //   }
-
-//   //   return user;
-//   // }
-
-//   static Future<User> signInWithGoogle() async {
-//     FirebaseAuth auth = FirebaseAuth.instance;
-//     User user;
-
-//     if (kIsWeb) {
-//       GoogleAuthProvider authProvider = GoogleAuthProvider();
-
-//       try {
-//         final UserCredential userCredential =
-//             await auth.signInWithPopup(authProvider);
-
-//         user = userCredential.user;
-//       } catch (e) {
-//         print(e);
-//       }
-//     } else {
-//       final GoogleSignIn googleSignIn = GoogleSignIn();
-
-//       final GoogleSignInAccount? googleSignInAccount =
-//           await googleSignIn.signIn();
-
-//       if (googleSignInAccount != null) {
-//         final GoogleSignInAuthentication googleSignInAuthentication =
-//             await googleSignInAccount.authentication;
-
-//         final AuthCredential credential = GoogleAuthProvider.credential(
-//           accessToken: googleSignInAuthentication.accessToken,
-//           idToken: googleSignInAuthentication.idToken,
-//         );
-
-//         try {
-//           final UserCredential userCredential =
-//               await auth.signInWithCredential(credential);
-
-//           user = userCredential.user;
-//         } on FirebaseAuthException catch (e) {
-//           if (e.code == 'account-exists-with-different-credential') {
-//             // ...
-//           } else if (e.code == 'invalid-credential') {
-//             // ...
-//           }
-//         } catch (e) {
-//           // ...
-//         }
-//       }
-//     }
-
-//     return user;
-//   }
-
-// }
 
 class Authentication {
   static Future<User> signInWithGoogle() async {
@@ -171,6 +22,14 @@ class Authentication {
             await auth.signInWithPopup(authProvider);
 
         user = userCredential.user;
+
+          assert(user.email != null);
+          assert(user.uid != null);
+          String gMail = user.email;
+          String iDuser = user.uid;
+          UserAuthbyGoogle(gMail, iDuser);
+
+
       } catch (e) {
         print(e);
       }
@@ -194,6 +53,14 @@ class Authentication {
               await auth.signInWithCredential(credential);
 
           user = userCredential.user;
+
+          assert(user.email != null);
+          assert(user.uid != null);
+          String gMail = user.email;
+          String iDuser = user.uid;
+          UserAuthbyGoogle(gMail, iDuser);
+
+
         } on FirebaseAuthException catch (e) {
           if (e.code == 'account-exists-with-different-credential') {
             print("Error de credenciales: Contraseña incorrecta");
